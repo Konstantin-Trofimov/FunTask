4cae0e89a078b56bfd2852352bf357f765695f5e
Konstantim <const.trofimov@gmail.com>
2020-09-02 15:54:17 +0300

    some fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 
+
 const cart = getArray('.cart'),
     box = getArray('.cart__box'),
     round = getArray('.cart__round'),
@@ -12,7 +13,7 @@ function getArray(elem) {
     return Array.from(document.querySelectorAll(elem));
 }
 
-document.querySelectorAll('.descr__ink').forEach((elem) => {
+document.querySelectorAll('.descr__link').forEach((elem) => {
     elem.addEventListener('click', (e) => {
         e.preventDefault();
         let attr = event.target.parentElement.getAttribute('data-taste');
@@ -25,62 +26,60 @@ cart.forEach((item) => {
     item.addEventListener('click', () => {
         let attr = item.getAttribute('data-taste');
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            switchCondition(item, attr)
+            switchCondition(item, attr);
         } else {
             item.onmouseleave = () => {
-                switchCondition(item, attr)
+                switchCondition(item, attr);
             }
         }
     });
 });
 
 function switchCondition(item, attr) {
-    changeSubtitle(item, attr);
-    if (item.classList.contains('is-default')) switchConditionToSelected(attr);
-    item.onclick = () => {
-        if (item.classList.contains('is-selected')) switchConditionToDisabled(attr);
-        else switchConditionBackToSelected(attr);
-    }
+    changeDescription(item, attr);
+    if (item.classList.contains('is-default')) toSelected(attr);
+    else if (item.classList.contains('is-selected')) toDisabled(attr);
+    else backToSelected(attr);
 }
 
-function switchConditionToSelected(attr) {
+function toSelected(attr) {
     selected(attr, cart, 'cart_selected');
     selected(attr, cart, 'is-selected');
     selected(attr, cart, 'cart_selected');
     selected(attr, box, 'cart__box_selected');
+    selected(attr, descrSelected, 'active');
     selected(attr, round, 'cart__round_selected');
     unSelected(attr, cart, 'is-default');
     unSelected(attr, descrDefault, 'active');
-    selected(attr, descrSelected, 'active');
 }
 
-function switchConditionToDisabled(attr) {
+function toDisabled(attr) {
     selected(attr, cart, 'cart_disabled');
     selected(attr, round, 'cart__round_disabled');
     selected(attr, box, 'cart__box_disabled');
+    selected(attr, cartSubtitle, 'active');
+    selected(attr, descrDisabled, 'active');
     unSelected(attr, cart, 'is-selected');
     unSelected(attr, cart, 'cart_selected');
     unSelected(attr, cart, 'cart__box_selected');
     unSelected(attr, round, 'cart__round_selected');
     unSelected(attr, cartSubtitleHover, 'active');
     unSelected(attr, descrSelected, 'active');
-    selected(attr, cartSubtitle, 'active');
-    selected(attr, descrDisabled, 'active');
 }
 
-function switchConditionBackToSelected(attr) {
+function backToSelected(attr) {
     selected(attr, cart, 'is-selected');
     selected(attr, cart, 'cart_selected');
     selected(attr, box, 'cart__box_selected');
     selected(attr, round, 'cart__round_selected');
+    selected(attr, descrSelected, 'active');
     unSelected(attr, cart, 'cart_disabled');
     unSelected(attr, box, 'cart__box_disabled');
     unSelected(attr, round, 'cart__round_disabled');
     unSelected(attr, descrDisabled, 'active');
-    selected(attr, descrSelected, 'active');
 }
 
-function changeSubtitle(item, attr) {
+function changeDescription(item, attr) {
     item.onmouseenter = () => {
         if (item.classList.contains('is-selected')) {
             selected(attr, cartSubtitleHover, 'active');
